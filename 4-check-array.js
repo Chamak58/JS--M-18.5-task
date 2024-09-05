35f3c87333290088589cf32e01b037a4e67bfa9d
Chamak Saha <chamaksaha58@gmail.com>
2024-09-05 21:27:59 +0600

    uploaded task: check-array.js file

---
+++ 4-check-array.js
@@ -0,0 +1,23 @@
+/**
+ *4. Checking if it's an Array
+Instructions:
+
+--Create different variables, each containing either an array or a non-array value.
+
+--Now use isArray to check if each variable is an array.
+
+--Print a message to the console indicating whether each variable is an array or not.
+*/
+const food = 'biriyani';
+const subjects = ['bangla', 'english', 'math', 'science', 'bgs'];
+const vehicle = 'bus';
+const weight = 62;
+const numbers = [45, 67, 34, 89, 90, 32];
+const success = true;
+
+console.log(Array.isArray(food));
+console.log(Array.isArray(subjects));
+console.log(Array.isArray(vehicle));
+console.log(Array.isArray(weight));
+console.log(Array.isArray(numbers));
+console.log(Array.isArray(success));
