336e347c2995b81750ee52a875959eb858aed05d
Chamak Saha <chamaksaha58@gmail.com>
2024-09-05 20:58:03 +0600

    uploaded task: add or remove element.js file

---
+++ 2-add-remove.js
@@ -0,0 +1,19 @@
+/**
+ * 2. Add or remove elements
+--Declare an array of 3 tourist destinations
+--Add a new tourist destination to your tourist array
+--Add two more to your array
+--Remove the last tourist destination you have added
+--display the final array as output
+*/
+let touristPlace = ['kaptai', 'cox-bazar', 'bandarban'];
+console.log(touristPlace);
+
+touristPlace.push('rangamati');
+console.log(touristPlace);
+
+touristPlace.push('sundarban', 'tanguar haor');
+console.log(touristPlace);
+
+touristPlace.pop();
+console.log(touristPlace);
