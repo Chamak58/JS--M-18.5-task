9a1b9c26cdab1913d6a2834fbbdea12faf04b02f
Chamak Saha <chamaksaha58@gmail.com>
2024-09-05 21:57:56 +0600

    uploaded task: combine array.js file

---
+++ 5-combine-array.js
@@ -0,0 +1,17 @@
+/**
+ 5. Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+ */
+
+let com1 = ['google', 'linkedin', 'ibm', 'apple', 'microsoft'];
+console.log(com1);
+
+let com2 = ['facebook', 'instagram', 'twiter'];
+let companies = com1.concat(com2);
+console.log(com2);
+
+console.log(companies);
