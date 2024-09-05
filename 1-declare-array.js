5f5ad2c56aa919b897f2fed2235e767e35b30c58
Chamak Saha <chamaksaha58@gmail.com>
2024-09-05 20:34:58 +0600

    uploaded task: declare-array.js file

---
+++ 1-declare-array.js
@@ -0,0 +1,19 @@
+/**
+ * 1. Declare an array
+1.Declare an array with 5 elements containing fruits
+2.console log the 3rd index element
+3.change the value of the 2nd index element to jambura
+4.console log the final array
+*/
+
+let fruits = ['mango', 'pineapple', 'guava', 'banana', 'papaya']
+console.log(fruits);
+console.log(fruits[3]);
+
+
+const fourth = fruits[3];
+console.log(fourth);
+
+//----------set / update value by index-------
+fruits[2] = 'jambura';
+console.log(fruits);
